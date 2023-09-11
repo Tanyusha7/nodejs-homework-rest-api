ebdf5d059ed28d117788c2ab92de228e34295280
Tanyusha7 <swanonlake@gmail.com>
2023-09-11 10:04:06 +0200

    Add mongoose

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-const app = require('./app')
+const app = require("./app");
+////-----
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require("mongoose");
+
+const DB_HOST =
+  "mongodb+srv://Tanya:aRtS5-jsLM4_qM7@cluster0.glkrgak.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => app.listen(3000))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// , () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
