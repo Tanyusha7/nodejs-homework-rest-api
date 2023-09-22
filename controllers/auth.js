00ae9dfc1d568956156005b6d0710b232d40978a
Tanyusha7 <swanonlake@gmail.com>
2023-09-22 20:46:14 +0200

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,27 +7,6 @@ const { User } = require("../models/user");
 
 const { ctrlWrapper, HttpError } = require("../helpers");
 
-// const payload = { id: "650960efdc99b01ff7511004" };
-// const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "20h" });
-// console.log(token);
-// const decodedToken = jwt.decode(token);
-// console.log(decodedToken);
-
-// try {
-//   const { id } = jwt.verify(
-//     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MDk2MGVmZGM5OWIwMWZmNzUxMTAwNCIsImlhdCI6MTY5NTEzNDI5OCwiZXhwIjoxNjk1MjA2Mjk4fQ.T4iJXVgxcw2vi2nESbJ0W0kHm3-rBBS1vdDSpMZUG",
-//     SECRET_KEY
-//   );
-//   console.log(id);
-// } catch (error) {
-//   console.log(error.message);
-// }
-
-// const creatHashPassword = async (password) => {
-//   const result = await bcrypt.hash(password, 10);
-//   const pas = await bcrypt.compare(password, result);
-// };
-
 const signup = ctrlWrapper(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
